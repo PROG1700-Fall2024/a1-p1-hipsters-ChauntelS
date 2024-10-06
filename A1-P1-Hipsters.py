a56277d9d6f3fe5755b65ff86237a1639ba430b1
Your Name <you@example.com>
2024-10-06 19:58:54 -0300

    Program 1 for assignment 1 is ready for marking.

--- A1-P1-Hipsters.py
+++ A1-P1-Hipsters.py
@@ -3,14 +3,35 @@
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
+    #Title
+    print("Hipster's Local Vinyl Records ""\nPurchase Invoice Record")
+    #instruction message
+    print("Please fill in the required fields below.")
 
+    #known variables
+    KilometerPrice = 15
+    TaxPrice = 0.14
 
+    #User input variables
+    RecoredPrice = float(input("Enter Record Sale Price:"))
+    KilometerAmount = float(input("Enter Distance in Kilometers:"))
+    CustomerName = input("Enter Customer's Name:")
 
+    #math
+    DeliveryPrice = float(KilometerPrice)*(KilometerAmount)
+    TaxAmount = float(RecoredPrice)*(TaxPrice)
+    RecoredTotal = float(RecoredPrice)+(TaxAmount)
+    TotalAmount = (float(RecoredTotal)+(DeliveryPrice))
 
-
-
-
-
+    #Outcome Summary with customer's name
+    print("Order Details for Customer:",end="")
+    print(CustomerName)
+    #print(DeliveryPrice)
+    print('Delivery Total:${0:.2f}'.format (DeliveryPrice)) 
+    #print(RecoredTotal)
+    print('Order Total:${0:.2f}'.format(RecoredTotal)) 
+    #Complete order total
+    print('Order Total including Delivery:${0:.2f}'.format(TotalAmount))   #<-- took me a while to remember how to get only 2 place holders after the decimal
     # YOUR CODE ENDS HERE
 
 main()
